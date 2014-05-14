1718ef6199bd0af0daff396fbb8b24d22c57f03e
Cole <Cole@HP-DESKTOP>
2014-05-14 18:42:16 -0400

    Add basic brick class

---
+++ brickbreaker/Brick.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package brickbreaker;
+
+import java.awt.*;
+
+/**
+ * The brick object
+ * @author Cole
+ */
+public class Brick extends Rectangle {
+    
+    /**
+     * Hit points left on the brick
+     */
+    private int health;
+    
+    /**
+     * The X and Y positions of the brick
+     */
+    private int xPos, yPos;
+    
+    /**
+     * Width of the brick
+     */
+    public final static int width = 15;
+    
+    /**
+     * Height of the brick
+     */
+    public final static int height = 5;
+    
+    /**
+     * Creates a brick at the specified coordinates
+     * @param x
+     * @param y 
+     */
+    public Brick(int x, int y) {
+        
+        //TODO: Code constructor
+        
+    }
+    
+}
